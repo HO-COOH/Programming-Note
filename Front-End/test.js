2cd26f68668b44c52ee7291b5504886238ffc9e2
HO-COOH <szpeter@yeah.net>
2020-04-16 13:36:38 -0500

    update python

--- Front-End/test.js
+++ Front-End/test.js
@@ -1,1 +1,7 @@
-console.log("helloworld".toUpperCase());
+function Person(fname, lname) {
+    this.fname=fname;
+    this.lname=lname;
+}
+
+const person1=Person('peter', 'li')
+console.log(person1)
