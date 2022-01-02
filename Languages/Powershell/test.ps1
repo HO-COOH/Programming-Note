08baf95c4146caa9e92e3536032a82f0b2bdf67a
HO-COOH <szpeter@yeah.net>
2022-01-03 03:07:30 +0800

    Add Windows.Uwp

--- Languages/Powershell/test.ps1
+++ Languages/Powershell/test.ps1
@@ -1,10 +1,1 @@
-using namespace System;
-using namespace System.IO;
-using namespace System.Diagnostics;
-
-$counter = [PerformanceCounter]::new("Processor", "% Processor Time", "_Total", [Environment]::MachineName);
-while ($true)
-{
-    [Console]::WriteLine($counter.NextValue());
-    [System.Threading.Thread]::Sleep(1000);
-}
+"Hello world"
